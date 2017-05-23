a0c1d180e32699d7a4373d7eb1d935321ee08fa3
pinguo-xuyihan <xuyihan@camera360.com>
2017-05-23 16:34:30 +0800

    a

--- server/sh/bannerAddP2.sh
+++ server/sh/bannerAddP2.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 
 PATH=/Users/yaweiyihan/Documents/libs/phantomjs-2.1.1-macosx/bin:$PATH
-cd /Users/yaweiyihan/Desktop/code/phantom/M1/banner
+cd /home/worker/data/www/processtest/testCases/M1/banner
 rm bannerAddP2.xml
 casperjs test bannerAddP2.js
